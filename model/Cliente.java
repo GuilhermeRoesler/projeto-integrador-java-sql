064e3c9219eaaac2e9061439a6b1b4b7ab0bedaf
Guilherme Roesler <guilhermeroesler.ss020@academico.ifsul.edu.br>
2024-10-15 12:16:42 -0300

    updating model

--- model/Cliente.java
+++ model/Cliente.java
@@ -1,5 +1,90 @@
 package model;
 
-public class Cliente {
+public class Cliente extends Pessoa {
+	protected String sobrenome;
+	protected String email;
+	protected String senha;
+	protected String sexo;
+	protected String cpf;
+	protected String telefone;
+	protected String cep;
+	protected String cidade;
+	
+	public Cliente(int id_pessoa, String nome, String nascimento, String sobrenome, String email, String senha,
+			String sexo, String cpf, String telefone, String cep, String cidade) {
+		super(id_pessoa, nome, nascimento);
+		this.sobrenome = sobrenome;
+		this.email = email;
+		this.senha = senha;
+		this.sexo = sexo;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.cep = cep;
+		this.cidade = cidade;
+	}
 
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
 }
--- model/Pessoa.java
+++ model/Pessoa.java
@@ -1,30 +1,14 @@
 package model;
+
 public class Pessoa {
 	protected int id_pessoa;
 	protected String nome;
-	protected String sobrenome;
-	protected String email;
-	protected String sexo;
-	protected String cpf;
-	protected String telefone;
 	protected String nascimento;
-	protected String senha;
-	protected String CEP;
-	protected String cidade;
-	
-	public Pessoa(int id_pessoa, String nome, String sobrenome, String email, String sexo, String cpf, String telefone,
-			String nascimento, String senha, String cep, String cidade) {
+
+	public Pessoa(int id_pessoa, String nome, String nascimento) {
 		this.id_pessoa = id_pessoa;
 		this.nome = nome;
-		this.sobrenome = sobrenome;
-		this.email = email;
-		this.sexo = sexo;
-		this.cpf = cpf;
-		this.telefone = telefone;
 		this.nascimento = nascimento;
-		this.senha = senha;
-		this.CEP = cep;
-		this.cidade = cidade;
 	}
 	
 	public Pessoa() {
@@ -47,46 +31,6 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
 	public String getNascimento() {
 		return nascimento;
 	}
@@ -95,31 +39,4 @@ public class Pessoa {
 		this.nascimento = nascimento;
 	}
 
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public String getCEP() {
-		return CEP;
-	}
-
-	public void setCEP(String cEP) {
-		CEP = cEP;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-	
-	
-	
-	
 }
