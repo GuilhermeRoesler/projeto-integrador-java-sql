8de49ebe6675c2d60dd5d342d229cc8df5fc00d3
Guilherme Roesler <guilhermeroesler.ss020@academico.ifsul.edu.br>
2024-10-15 09:33:06 -0300

    Update Main.java

--- apresentacao/Main.java
+++ apresentacao/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			JFramePrincipal frame = new JFramePrincipal();
+			JFrameCadastro frame = new JFrameCadastro();
 			frame.setLocationRelativeTo(null);
 			frame.setResizable(false);
 			frame.setVisible(true);
